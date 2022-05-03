f73c77f799d80e3f4c2e54815f464796bf244b83
Axel Arbet-Engels <aarbet@mpp.mpg.de>
2022-05-03 12:00:55 +0200

    minor fix

--- LC_simulations.py
+++ LC_simulations.py
@@ -537,7 +537,7 @@ class lightcurve:
 			true_beta_mjd_sim = true_beta_sim_LC[0]
 			true_beta_flux_sim = true_beta_sim_LC[1][0]
 
-			best_fit_beta = self.estimate_PSD_MFVF(N_sim_LC, N_LC_sim_length_mult, LC_sim_time_precision, LC_output_t_bin, mfvf_min_time, mfvf_binning, beta_range, output_fig_name=fig_name+str(i)+'.png', true_beta_LC_mjd=true_beta_mjd_sim, true_beta_LC_flux=true_beta_flux_sim, Em_method, PDF_skewnorm_param)
+			best_fit_beta = self.estimate_PSD_MFVF(N_sim_LC, N_LC_sim_length_mult, LC_sim_time_precision, LC_output_t_bin, mfvf_min_time, mfvf_binning, beta_range, output_fig_name=fig_name+str(i)+'.png', true_beta_LC_mjd=true_beta_mjd_sim, true_beta_LC_flux=true_beta_flux_sim, Em_method=Em_method, PDF_skewnorm_param=PDF_skewnorm_param)
 			fitted_beta.append(best_fit_beta)
 		
 		#get uncertainty bands
